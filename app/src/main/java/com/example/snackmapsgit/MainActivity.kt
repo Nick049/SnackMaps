cee69bac94ce988f3df4b635ec77c1c180bd34df
MartinMWTC <112421890+MartinMWTC@users.noreply.github.com>
2023-10-11 11:43:59 -0400

    fix for build

--- app/src/main/java/com/example/snackmapsgit/MainActivity.kt
+++ app/src/main/java/com/example/snackmapsgit/MainActivity.kt
@@ -37,7 +37,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SnackMapsGitTheme {
