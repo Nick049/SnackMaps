bebac4e7c48ae6ba37be5e0158309cbe6223fe4b
MartinMWTC <112421890+MartinMWTC@users.noreply.github.com>
2023-10-11 14:00:08 -0400

    end of day - ui started
    got things to print to screen, started on actual function to print products after being scraped

--- app/src/main/java/com/example/snackmapsgit/MainActivity.kt
+++ app/src/main/java/com/example/snackmapsgit/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.snackmapsgit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,6 +13,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.snackmapsgit.ui.theme.SnackMapsGitTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,26 +36,55 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ) {}
+                TestText()
+                TestText()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TestText() {
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            //prints image file in drawables
+            painter = painterResource(R.drawable.doritos),
+            contentDescription = "doritos",
+            modifier = Modifier
+            //sets image size
+            .size(150.dp)
+        )
+        //created space between image and text
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text("Doritos")
+            Text("Nacho Cheese")
+            Text("Location: walmart")
+        }
+    }
 }
 
-//@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SnackMapsGitTheme {
-        Greeting("Android")
+fun ProductPlacer(product: String, type: String, location: String, address: String) {
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            //prints image file in drawables
+            painter = painterResource(R.drawable.doritos),
+            contentDescription = "doritos",
+            modifier = Modifier
+                //sets image size
+                .size(150.dp)
+        )
+        //created space between image and text
+        Spacer(modifier = Modifier.width(10.dp))
+        Column {
+            Text("$product")
+            Text("$type")
+            Text("$location")
+            Text( "$address")
+        }
     }
+
+
 }
